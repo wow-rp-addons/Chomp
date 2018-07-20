578c25e96f7b38154c06dca03e8994835e7486b1
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2018-07-20 15:51:36 -0400

    Update version, TOC

--- Internal.lua
+++ Internal.lua
@@ -14,7 +14,7 @@
 	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]
 
-local VERSION = 4
+local VERSION = 5
 
 if IsLoggedIn() then
 	error(("Chomp Message Library (embedded: %s) cannot be loaded after login."):format((...)))
