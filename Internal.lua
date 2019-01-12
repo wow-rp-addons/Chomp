2235cfb7295a865465974c4b3884a21c631e5aaf
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2019-01-12 10:09:34 -0500

    Version bump
    For build 28807, 8.1.0

--- Internal.lua
+++ Internal.lua
@@ -14,7 +14,7 @@
 	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]
 
-local VERSION = 7
+local VERSION = 8
 
 if IsLoggedIn() then
 	error(("Chomp Message Library (embedded: %s) cannot be loaded after login."):format((...)))
