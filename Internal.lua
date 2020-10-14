a92393b087639fab5654c9b89ea7f6cec1e7e368
Solanya <paul.corlay@gmail.com>
2020-10-14 18:33:19 +0200

    Version 13

--- Internal.lua
+++ Internal.lua
@@ -14,7 +14,7 @@
 	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]
 
-local VERSION = 12
+local VERSION = 13
 
 if IsLoggedIn() then
 	error(("Chomp Message Library (embedded: %s) cannot be loaded after login."):format((...)))
