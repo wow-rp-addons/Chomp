9905f999f9fbe9b51bad021b40e09575551c99b4
Daniel Yates <me@meorawr.io>
2021-08-30 21:23:52 +0100

    Bump version to 19
    Also bump the TOC for standalone installs on BCC.

--- Internal.lua
+++ Internal.lua
@@ -14,7 +14,7 @@
 	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]
 
-local VERSION = 18
+local VERSION = 19
 
 if IsLoggedIn() then
 	error(("Chomp Message Library (embedded: %s) cannot be loaded after login."):format((...)))
