ca0ba6b09f162e70e61a415c3e8fa7315758b8d4
Daniel Yates <me@meorawr.io>
2024-05-07 14:03:19 +0100

    Up we go

--- Internal.lua
+++ Internal.lua
@@ -14,7 +14,7 @@
 	CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ]]
 
-local VERSION = 27
+local VERSION = 28
 
 if IsLoggedIn() then
 	error(("Chomp Message Library (embedded: %s) cannot be loaded after login."):format((...)))
